37b44bd582f04bb555dcb6a0a96673a233a1406d
minhbui <buiphuocminh94a@gmail.com>
2024-01-08 12:42:04 +0700

    fresh config

---
+++ lua/custom/chadrc.lua
@@ -0,0 +1,10 @@
+---@type ChadrcConfig
+local M = {}
+
+M.ui = { 
+  theme = 'onedark',
+  blankline = { style = "rainbow", blank = true }, -- style: rainbow/nil, blank = true/false
+  statusline = {theme='vscode'},
+}
+
+return M
